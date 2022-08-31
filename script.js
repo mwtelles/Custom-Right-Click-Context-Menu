10632922e29a9a4c16c0e21b379b041220f7cf77
Matheus Telles <business.mwtelles@gmail.com>
2022-08-31 14:54:53 -0300

    Localizando o click em qualquer local da tela
    Tratando o comportamento do menu no limite da tela.

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
 const contextMenu = document.querySelector('.wrapper');
 
+shareMenu = contextMenu.querySelector('.share-menu');
+
 document.addEventListener('contextmenu', e => {
-    console.log('Right Click')
-})
+    e.preventDefault();
+
+    let x = e.offsetX, y = e.offsetY;
+
+    winWidth = window.innerWidth;
+    winHeight = window.innerHeight;
+    cmWidth = contextMenu.offsetWidth;
+    cmHeight = contextMenu.offsetHeight;
+
+    if (x > (winWidth - cmWidth - shareMenu.offsetWidth)) {
+        shareMenu.style.left = "-200px";
+    }
+
+    x = x > winWidth - cmWidth ? winWidth - cmWidth : x;
+    y = y > winHeight - cmHeight ? winHeight - cmHeight : y;
+
+    contextMenu.style.left = `${x}px`;
+    contextMenu.style.top = `${y}px`;
+
+    contextMenu.style.visibility = 'visible';
+});
